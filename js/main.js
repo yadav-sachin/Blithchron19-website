73b84a814124adb9295787d90088f71f1cb74b3a
Sachin Yadav <sachinyadav7024@gmail.com>
2019-06-29 21:11:21 +0530

    add basic countdown

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*This code is for Count Down Timer*/
 var countDownDate = new Date("Jan 11, 2020 00:00:00").getTime();
 function showTime()
 {
@@ -6,11 +7,22 @@ function showTime()
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     console.log(days);
+    var timeLeft=document.getElementById("time-left");
+    if(days>0)
+    {   
+        timeLeft.innerText=days + " days Left";
+    }
+    else if(hours>0)
+        timeLeft.innerText=hours+ " hours Left";
+    else timeLeft.innerText="Blith is ON";
+    console.log(timeLeft.innerText);
 }
 function start()
 {   
     showTime();
     var x= setInterval(showTime,60000);
 }
+
+/*Countdown code ends here*/
 
 $(document).ready(start);
