ac4735051884c9293af42a1f775e726581545c4c
Sachin Yadav <sachinyadav7024@gmail.com>
2019-07-07 16:35:34 +0530

    make title resposnive,change color scheme, add title text animation

--- js/main.js
+++ js/main.js
@@ -30,6 +30,8 @@ function start()
 {   
     showTime();
     var x= setInterval(showTime,60000);
+    $(".title").lettering();
+    $(".button").lettering();
     $(".header__logo").hover(function(){
         $(this).addClass("animated rubberBand");},function(){
         $(this).removeClass("animated rubberBand");});
@@ -41,3 +43,19 @@ function start()
 
 /*Countdown code ends here*/
 $(document).ready(start);
+$(document).ready(function() {
+    animation();
+  }, 1000);
+
+$('.button').click(function() {
+animation();
+});
+
+function animation() {
+  var title1 = new TimelineMax();
+  title1.to(".button", 0, {visibility: 'hidden', opacity: 0})
+  title1.staggerFromTo(".title span", 0.5, 
+  {ease: Back.easeOut.config(1.7), opacity: 0, bottom: -80},
+  {ease: Back.easeOut.config(1.7), opacity: 1, bottom: 0}, 0.05);
+  title1.to(".button", 0.2, {visibility: 'visible' ,opacity: 1})
+}
